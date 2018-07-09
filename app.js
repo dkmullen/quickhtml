d422327ae856f6dd1fdb8b4f70197ef4c5887c5e
Dennis Mullen <dkmullen@gmail.com>
2018-07-09 07:02:55 -0400

    Fix alignment issues for main nav

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function toggleDisplay() {
     let vis = document.getElementById('main-nav')
     vis.style.display === '' || vis.style.display === 'none' 
-        ? vis.style.display = 'block' 
+        ? vis.style.display = 'inline-block' 
         : vis.style.display = 'none';  
     
 }
